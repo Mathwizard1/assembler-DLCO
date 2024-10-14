61200bca8f078239b644ead2d8ffb27e28909074
Mathwizard1 <anshurup.gupta@gmail.com>
2024-10-15 00:51:10 +0530

    basic files

--- test.asm
+++ test.asm
@@ -2,10 +2,11 @@
 label:  ; an unused label 
  ldc 0 
  ldc -5 
- ldc +5 
-loop: br loop ; an infinite loop 
+ ldc +5
+add 
+loop: br loop ; an infinite loop
 br next ;offset should be zero 
 next: 
- ldc  loop ; load code address 
- ldc var1 ; forward ref 
+ldc loop ; load code address 
+ldc var1 ; forward ref 
 var1: data 0 ; a variable
